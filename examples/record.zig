2e898144253118e55811e96b5badb802cc317558
Stephen Gutekanst <stephen@hexops.com>
2023-07-16 12:11:04 -0700

    examples: have record example write to file and give instructions
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- examples/record.zig
+++ examples/record.zig
@@ -1,16 +1,10 @@
-//! Redirects input device into stdout.
-//! You may write the output into a file
-//! and use one of the following tools to hear it:
-//!
-//! ```
-//! paplay -p --format=FLOAT32LE --rate 48000 --raw ./audio
-//! aplay -c 2 -f FLOAT_LE -r 48000 ./audio
-//! ```
+//! Redirects input device into ./raw_audio file.
 
 const std = @import("std");
 const sysaudio = @import("sysaudio");
 
 var recorder: sysaudio.Recorder = undefined;
+var file: std.fs.File = undefined;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -26,11 +20,30 @@ pub fn main() !void {
     defer recorder.deinit();
     try recorder.start();
 
+    file = try std.fs.cwd().createFile("raw_audio", .{});
+
+    std.debug.print("Recording to ./raw_audio using:\n", .{});
+    std.debug.print("\n", .{});
+    std.debug.print("  device: {s}\n", .{device.name});
+    std.debug.print("  channels: {}\n", .{device.channels.len});
+    std.debug.print("  sample_rate: {}\n", .{recorder.sampleRate()});
+    std.debug.print("\n", .{});
+    std.debug.print("You can play this recording back using e.g.:\n", .{});
+    std.debug.print("\n", .{});
+    std.debug.print("  $ ffplay -f f32le -ar {} -ac {} raw_audio", .{recorder.sampleRate(), device.channels.len});
+    std.debug.print("\n", .{});
+    // Note: you may also use e.g.:
+    //
+    // ```
+    // paplay -p --format=FLOAT32LE --rate 48000 --raw ./audio
+    // aplay -c 2 -f FLOAT_LE -r 48000 ./audio
+    // ```
+
     while (true) {}
 }
 
 fn readCallback(_: ?*anyopaque, frames: usize) void {
-    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    var bw = std.io.bufferedWriter(file.writer());
     for (0..frames) |fi| {
         for (recorder.channels()) |ch| {
             const sample = recorder.read(ch, fi, f32);
